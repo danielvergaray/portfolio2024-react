3142dce34635e60c777cd30e0e484cc7153c5745
Daniel Vergaray <danielvergarayescobar@gmail.com>
2024-02-14 16:14:59 -0500

    aspecto visual de la seccion de portafolio

--- src/components/portfolio/PortfolioList.jsx
+++ src/components/portfolio/PortfolioList.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-const PortfolioList = ({infoProyectos}) => {
+const PortfolioList = ({ infoProyectos, infoMenu }) => {
+ 
+const [proyectos, setProyectos]= useState("")
+
+  const handleClick = (opcionMenu) => {
+    
+    console.log(opcionMenu.opcionMenu.titulo);
+  };
 
   return (
-    <section className='portfolio-proyectos'>
-        
-        {infoProyectos.map((proyecto)=>(
-            <div className='portfolio-proyectos_recuadro'>
-                <h3 className='oculto'>{proyecto.titulo} </h3>
-                <p className='oculto'>{proyecto.descripcionBreve} </p>
-            </div>
-        ))
-        
-        
-        }
-    </section>
-  )
-}
+    <>
+      <article className="portfolio-proyectos_barraMenu">
+        {infoMenu.map((opcionMenu) => (
+          <li onClick={()=> handleClick({opcionMenu})} 
+          > {opcionMenu.titulo} </li>
+        ))}
+      </article>
+      <section className="portfolio-proyectos">
+        {infoProyectos.map((proyecto) => (
+          <div
+            style={{ backgroundImage: `url(${proyecto.imagenRecuadro})` }}
+            className="portfolio-proyectos_recuadro"
+          >
+            <h3 className="oculto">{proyecto.titulo} </h3>
+            <p className="oculto">{proyecto.descripcionBreve} </p>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
 
-export default PortfolioList
+export default PortfolioList;
--- src/components/portfolio/PortfolioListContainer.jsx
+++ src/components/portfolio/PortfolioListContainer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import PortfolioList from "./PortfolioList";
+import ImagenAdivina from "../../assets/imagenes/proyectos/adivinaElNumero.jpeg";
+import ImagenMrSnackEcommerce from "../../assets/imagenes/proyectos/ecommerce.jpeg";
+import ImagenPigGame from "../../assets/imagenes/proyectos/pigGame.jpg";
+import ImagenMrSnackWeb from "../../assets/imagenes/proyectos/web-mr-snack.jpeg";
+import ImagenGameStage from "../../assets/imagenes/proyectos/react-gameStage.jpeg";
+import ImagenViajesWp from "../../assets/imagenes/proyectos/wp-blog.jpeg";
+import ImagenCentroMedicoWp from "../../assets/imagenes/proyectos/centroMedicoWp.jpeg";
 
 const PortfolioListContainer = () => {
   const cursos = [
@@ -54,70 +61,97 @@ const PortfolioListContainer = () => {
   const proyectos = [
     {
       titulo: "Adivina el Número",
+      imagenRecuadro: ImagenAdivina,
       descripcionBreve:
         "Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.",
       imagen: "",
+      category: "javascript",
       linkWeb: "https://danielvergaray.github.io/AdivinaElNumero_juego/",
       linkRepositorio:
         "https://github.com/danielvergaray/AdivinaElNumero_juego",
     },
     {
       titulo: "Ecommerce Mr. Snack",
+      imagenRecuadro: ImagenMrSnackEcommerce,
       descripcionBreve:
         "Este proyecto consiste en un Ecommerce creado para el emprendimiento Mr. Snack. Ha sido realizado utilizando javaScript, creando los productos del emprendimiento de manera dinamica pudiendo filtrar los productos por categoria y buscarlos por su nombre, se realizaron versiones desktop y mobile.",
       imagen: "",
-      linkWeb: "https://danielvergaray.github.io/entregaFinal_ecommerce/index.html",
+      category: "javascript",
+      linkWeb:
+        "https://danielvergaray.github.io/entregaFinal_ecommerce/index.html",
       linkRepositorio:
         "https://github.com/danielvergaray/entregaFinal_ecommerce",
     },
     {
-        titulo: "Juego: Pig Game",
-        descripcionBreve:
-          "Este proyecto consiste en un juego donde se le permite al usuario lanzar el dado y sumar los numeros obtenidos de manera aleatoria hasta llegar al objetivo. Fue realizado usando javaScript.",
-        imagen: "",
-        linkWeb: "https://danielvergaray.github.io/pig-game/",
-        linkRepositorio:
-          "https://github.com/danielvergaray/pig-game",
-      },
-      {
-        titulo: "Web Mr. Snack",
-        descripcionBreve:
-          "Este proyecto realizado para un emprendimiento de snacks. Fue llevado a cabo usando Html5 y Css3, ademas se implementaron estategias SEO, Bootstrap y Sass, se realizaron versiones desktop y mobile.",
-        imagen: "",
-        linkWeb: "https://danielvergaray.github.io/pagina-web-mrSnack/",
-        linkRepositorio:
-          "https://github.com/danielvergaray/pagina-web-mrSnack",
-      },
-      {
-        titulo: "Ecommerce GameStage",
-        descripcionBreve:
-          "Este proyecto consiste en un Ecommerce creado como una simulación de una tienda de videojuegos. Ha sido realizado utilizando React, creando los productos del emprendimiento de manera dinamica pudiendo filtrar los productos por categoria y buscarlos por su nombre, los productos fueron almacenados en una base de datos externa (API), se realizaron versiones desktop y mobile.",
-        imagen: "",
-        linkWeb: "https://ephemeral-taffy-ada997.netlify.app/",
-        linkRepositorio:
-          "https://github.com/danielvergaray/react_ecommerce",
-      },
-      {
-        titulo: "Blog de viajes/ecommerce",
-        descripcionBreve:
-          "Este proyecto consiste en un blog de viajes, incluyendo tienda virtual, se ha realizado usando WordPress, plugings, elementor, se realizaron versiones desktop y mobile.",
-        imagen: "",
-        linkWeb: "https://dev-elviajededaniel.pantheonsite.io/",
-        linkRepositorio:
-          "",
-      },
-      {
-        titulo: "Web Centro odontologico",
-        descripcionBreve:
-          "",
-        imagen: "",
-        linkWeb: "https://dev-centromedicodental.pantheonsite.io/",
-        linkRepositorio:
-          "",
-      },
+      titulo: "Juego: Pig Game",
+      imagenRecuadro: ImagenPigGame,
+      descripcionBreve:
+        "Este proyecto consiste en un juego donde se le permite al usuario lanzar el dado y sumar los numeros obtenidos de manera aleatoria hasta llegar al objetivo. Fue realizado usando javaScript.",
+      imagen: "",
+      category: "javascript",
+      linkWeb: "https://danielvergaray.github.io/pig-game/",
+      linkRepositorio: "https://github.com/danielvergaray/pig-game",
+    },
+    {
+      titulo: "Web Mr. Snack",
+      imagenRecuadro: ImagenMrSnackWeb,
+      descripcionBreve:
+        "Este proyecto realizado para un emprendimiento de snacks. Fue llevado a cabo usando Html5 y Css3, ademas se implementaron estategias SEO, Bootstrap y Sass, se realizaron versiones desktop y mobile.",
+      imagen: "",
+      category: "desarrollo",
+      linkWeb: "https://danielvergaray.github.io/pagina-web-mrSnack/",
+      linkRepositorio: "https://github.com/danielvergaray/pagina-web-mrSnack",
+    },
+    {
+      titulo: "Ecommerce GameStage",
+      imagenRecuadro: ImagenGameStage,
+      descripcionBreve:
+        "Este proyecto consiste en un Ecommerce creado como una simulación de una tienda de videojuegos. Ha sido realizado utilizando React, creando los productos del emprendimiento de manera dinamica pudiendo filtrar los productos por categoria y buscarlos por su nombre, los productos fueron almacenados en una base de datos externa (API), se realizaron versiones desktop y mobile.",
+      imagen: "",
+      category: "react",
+      linkWeb: "https://ephemeral-taffy-ada997.netlify.app/",
+      linkRepositorio: "https://github.com/danielvergaray/react_ecommerce",
+    },
+    {
+      titulo: "Blog de viajes/ecommerce",
+      imagenRecuadro: ImagenViajesWp,
+      descripcionBreve:
+        "Este proyecto consiste en un blog de viajes, incluyendo tienda virtual, se ha realizado usando WordPress, plugings, elementor, se realizaron versiones desktop y mobile.",
+      imagen: "",
+      category: "wordpress",
+      linkWeb: "https://dev-elviajededaniel.pantheonsite.io/",
+      linkRepositorio: "",
+    },
+    {
+      titulo: "Web Centro odontologico",
+      imagenRecuadro: ImagenCentroMedicoWp,
+      descripcionBreve: "",
+      imagen: "",
+      category: "wordpress",
+      linkWeb: "https://dev-centromedicodental.pantheonsite.io/",
+      linkRepositorio: "",
+    },
   ];
 
-  return <PortfolioList infoProyectos={proyectos} />;
+  const menuProyectos = [
+    {
+      titulo: "TODOS",
+    },
+    {
+      titulo: "REACT",
+    },
+    {
+      titulo: "JAVASCRIPT",
+    },
+    {
+      titulo: "DESARROLLO WEB",
+    },
+    {
+      titulo: "WORDPRESS",
+    },
+  ];
+
+  return <PortfolioList infoProyectos={proyectos} infoMenu={menuProyectos} />;
 };
 
 export default PortfolioListContainer;
